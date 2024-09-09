01100aab16067980f398b6994bd8196547e5f7ca
Dave Dalcino <ddalcino@gmail.com>
2024-09-09 12:40:28 -0700

    refactor CachedDirectory
    Don't use Dicts when they aren't needed and aren't type-safe.
    Keep private methods private.

--- src/cached_directory.py
+++ src/cached_directory.py
@@ -1,53 +1,60 @@
 import logging
 from pathlib import Path
 import json
-from typing import Dict, Literal, Set
+from typing import Set
 
 
 class CachedDirectory:
-    ToolsQtSet = Dict[Literal['tools', 'qt'], Set[str]]
     def __init__(self, cached_dir_path: Path, indent: int = 1):
         self.indent = indent
         self.cached_dir_path = cached_dir_path
-        self.directory = self.cached_dir_path / 'directory.json'
+        self.directory = self.cached_dir_path / "directory.json"
         if not cached_dir_path.exists():
             cached_dir_path.mkdir(parents=True)
         if self.directory.exists():
-            self.previous_dir: CachedDirectory.ToolsQtSet = json.loads(cached_dir_path.read_text())
+            previous_cache = json.loads(self.directory.read_text())
+            self._previous_tools = set(previous_cache["tools"])
+            self._previous_qt = set(previous_cache["qt"])
         else:
-            self.previous_dir: CachedDirectory.ToolsQtSet = {'tools': set(), 'qt': set()}
-        self.new_dir: CachedDirectory.ToolsQtSet = {'tools': set(), 'qt': set()}
+            self._previous_tools, self._previous_qt = set(), set()
+        self._new_tools, self._new_qt = set(), set()
 
     def use_cached_folder(self, folder: str):
-        which = CachedDirectory.which(folder)
-        self.new_dir[which].update(entry for entry in self.previous_dir[which] if entry.startswith(folder))
+        if folder.startswith("qt"):
+            previous_folder_set, new_folder_set = self._previous_qt, self._new_qt
+        else:
+            previous_folder_set, new_folder_set = self._previous_tools, self._new_tools
+        new_folder_set.update(entry for entry in previous_folder_set if entry.startswith(folder))
 
     def out(self) -> str:
-        return json.dumps(
-            {'qt': sorted(self.new_dir['qt']), 'tools': sorted(self.new_dir['tools'])},
-            indent=self.indent
-        )
+        return json.dumps({"qt": sorted(self._new_qt), "tools": sorted(self._new_tools)}, indent=self.indent)
 
     def save(self):
         self.directory.write_text(self.out())
 
     def add_folder(self, folder: str):
-        self.new_dir[CachedDirectory.which(folder)].add(folder)
+        self._new_set(folder).add(folder)
 
-    @staticmethod
-    def which(folder: str) -> Literal['qt', 'tools']:
-        return 'qt' if folder.startswith('qt') else 'tools'
+    def _previous_set(self, folder: str) -> Set[str]:
+        """mutable reference to self._previous_qt or self._previous_tools"""
+        return self._previous_qt if folder.startswith("qt") else self._previous_tools
+
+    def _new_set(self, folder: str) -> Set[str]:
+        """mutable reference to self._new_qt or self._new_tools"""
+        return self._new_qt if folder.startswith("qt") else self._new_tools
 
     def __contains__(self, folder: str) -> bool:
-        which = CachedDirectory.which(folder)
-        for f in self.previous_dir[which]:
-            if f == folder or f.startswith(folder + '/'):
+        for f in self._previous_set(folder):
+            if f == folder or f.startswith(folder + "/"):
                 return True
 
     def prune_removed_files(self, logger: logging.Logger):
-        for which in ('qt', 'tools'):
-            for folder in self.previous_dir[which]:
-                if folder not in self.new_dir[which]:
-                    json_file = self.cached_dir_path / f'{folder}.json'
+        for previous_folder_set, new_folder_set in (
+            (self._previous_qt, self._new_qt),
+            (self._previous_tools, self._new_tools),
+        ):
+            for previous_folder in previous_folder_set:
+                if previous_folder not in new_folder_set:
+                    json_file = self.cached_dir_path / f"{previous_folder}.json"
                     logger.info(f"Removing {json_file}")
                     json_file.unlink()
